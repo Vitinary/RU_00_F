a15a06b1b772e40f9a27659af6110b795d9c5a46
Vitinary <153402650+Vitinary@users.noreply.github.com>
2026-02-13 05:31:37 +0300

    Add files via upload

--- js/quizer.js
+++ js/quizer.js
@@ -889,6 +889,11 @@ let ru_2000_f = [
 		pack : RU_2000_F_PACK_3,
 		group : 'Агния Дитковските',
 		song : "Самолёты (ft Рома Кенга) (2009)"
+	},
+	{
+		pack : RU_2000_F_PACK_2,
+		group : 'Ангина',
+		song : "Твоя девочка ушла (2004)"
 	}
 ];
 
@@ -927,6 +932,7 @@ function map_songs(){
 	$('#mirror').hide();
 	$('#map').hide();
 	$('#package_content').hide();
+	$('#sec_15_hist').show();
 	$('#mapping_content').show();
 	toggleLearn();
 	for(var j=0; j < music.length; j++){
@@ -1265,6 +1271,15 @@ function back_to_browser(){
 function back_to_current_pack(){
 	back = back_to_browser;
 	$('#mapping_content').hide();
+	$('#sec_15_hist').hide();
+	song_stop();
 	$('#map').show();
 	package_num(pack_num);
+}
+
+function song_stop() {
+	if(audio){
+		audio.pause();
+		audio = null;
+	}
 }
